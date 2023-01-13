33e98b811d323e72c5e48e13ceeccb78e0a97a80
Quinlan <quinlan@akali>
2023-01-13 20:20:41 +0700

    Finished Blogs Routes

--- routes/blogs.js
+++ routes/blogs.js
@@ -45,22 +45,103 @@ router.post("/", async (req, res) => {
 
   try {
     const newBlog = await blog.save();
-    // res.redirect('blogs/${newBlog.id}');
-    res.redirect("blogs");
+    res.redirect(`blogs/${newBlog.id}`);
   } catch {
     renderNewPage(res, blog, true);
   }
 });
 
+// Show Blog Route
+router.get("/:id", async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id).populate("author").exec();
+    res.render("/blogs/show", { blog: blog });
+  } catch {
+    res.redirect("/");
+  }
+});
+
+// Edit Blog Route
+router.get("/:id/edit", async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+    renderEditPage(res, blog);
+  } catch {
+    res.redirect("/");
+  }
+});
+
+// Update Blog Route
+router.put("/:id", async (req, res) => {
+  // const blog = new Blog({
+  //   title: req.body.title,
+  //   author: req.body.author,
+  //   // publishDate: new Date(req.body.publishDate), ??? NEEDS CONSIDERATION
+  //   // commentCount: req.body.commentCount, ??? NEEDS CONSIDERATION
+  //   description: req.body.description,
+  // });
+  let blog;
+  try {
+    blog = await Blog.findById(req.params.id);
+    blog.title = req.body.title;
+    blog.author = req.body.author;
+    blog.description = req.body.description;
+    blog.modifiedAt = new Date(Date.now);
+    if (req.body.image != null && req.body.image !== "") {
+      saveImage(blog, req.body.image);
+    }
+    await blog.save();
+    res.redirect(`/blogs/${blog.id}`);
+  } catch {
+    if (blog != null) {
+      renderEditPage(res, blog, true);
+    } else {
+      redirect("/");
+    }
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  let blog;
+  try {
+    blog = await Blog.findById(req.params.id);
+    await blog.remove();
+    res.redirect("/blogs");
+  } catch {
+    if (blog != null) {
+      res.render("blogs/show", {
+        blog: blog,
+        errorMessage: "Could not remove blog",
+      });
+    } else {
+      res.redirect("/");
+    }
+  }
+});
+
 async function renderNewPage(res, blog, hasError = false) {
+  renderFormPage(res, blog, "new", hasError);
+}
+
+async function renderEditPage(res, blog, hasError = false) {
+  renderFormPage(res, blog, "edit", hasError);
+}
+
+async function renderFormPage(res, blog, form, hasError = false) {
   try {
     const authors = await Author.find({});
     const params = {
       authors: authors,
       blog: blog,
     };
-    if (hasError) params.errorMessage = "Error Creating Blog";
-    res.render("blogs/new", params);
+    if (hasError) {
+      if (form === "edit") {
+        params.errorMessage = "Error Updating Blog";
+      } else {
+        params.errorMessage = "Error Creating Blog";
+      }
+    }
+    res.render(`blogs/${form}`, params);
   } catch {
     res.redirect("/blogs");
   }
